200a90b68af1121b4799afc577dded2be324138b
mkrol1997 <golotka333@gmail.com>
2024-01-28 03:35:40 +0100

    Added part category field validation

--- elmark_warehouse/warehouse_api/serializers.py
+++ elmark_warehouse/warehouse_api/serializers.py
@@ -1,9 +1,8 @@
 from typing import Any, Optional, OrderedDict
 
+from API.models import Categories, PartLocation, Parts
+from API.validators import CategoriesValidators, PartsValidators
 from rest_framework_mongoengine import serializers
-
-from .models import Categories, PartLocation, Parts
-from .validators import CategoriesValidators, PartsValidators
 
 
 class CategoriesSerializer(serializers.DocumentSerializer):
@@ -33,6 +32,11 @@ class PartsSerializer(serializers.DocumentSerializer):
     class Meta:
         model = Parts
         fields = ["serial_number", "name", "description", "category", "quantity", "price", "location"]
+
+    def validate_category(self, category_name: str) -> Optional[str]:
+        PartsValidators.validate_part_category_exists(category_name)
+        PartsValidators.validate_is_part_category_subcategory(category_name)
+        return category_name
 
     def to_representation(self, instance) -> OrderedDict[Any, Any | None]:
         queryset = super().to_representation(instance)
