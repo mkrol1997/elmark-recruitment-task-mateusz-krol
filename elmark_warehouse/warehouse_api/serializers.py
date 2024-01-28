fab17ec212ae704fe24274978cbbff37a9bb6ae8
mkrol1997 <golotka333@gmail.com>
2024-01-28 03:34:38 +0100

    Added parent_name field validation

--- elmark_warehouse/warehouse_api/serializers.py
+++ elmark_warehouse/warehouse_api/serializers.py
@@ -1,14 +1,19 @@
-from typing import Any, OrderedDict
+from typing import Any, Optional, OrderedDict
 
 from rest_framework_mongoengine import serializers
 
 from .models import Categories, PartLocation, Parts
+from .validators import CategoriesValidators, PartsValidators
 
 
 class CategoriesSerializer(serializers.DocumentSerializer):
     class Meta:
         model = Categories
         fields = ["name", "parent_name"]
+
+    def validate_parent_name(self, parent_name: str) -> Optional[str]:
+        CategoriesValidators.validate_parent_name(parent_name)
+        return parent_name
 
     def to_representation(self, instance) -> OrderedDict[Any, Any | None]:
         queryset = super().to_representation(instance)
