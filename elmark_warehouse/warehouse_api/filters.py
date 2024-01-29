7dd1b71ca23c0bfb1537cb3204a5c7a97fb920b3
mkrol1997 <golotka333@gmail.com>
2024-01-29 01:59:09 +0100

    Modified location fields filter label

--- elmark_warehouse/warehouse_api/filters.py
+++ elmark_warehouse/warehouse_api/filters.py
@@ -14,8 +14,17 @@ class CategoriesFilter(FilterSet):
 
 class PartsFilter(FilterSet):
     filter_overrides = settings.FILTER_CONFIG
-    price = filters.RangeFilter()
-    quantity = filters.RangeFilter()
+
+    price_min = filters.NumberFilter(name="price", label="price_min", lookup_type="gte")
+    price_max = filters.NumberFilter(name="price", label="price_max", lookup_type="lte")
+    quantity_min = filters.NumberFilter(name="quantity", label="quantity_min", lookup_type="gte")
+    quantity_max = filters.NumberFilter(name="quantity", label="quantity_max", lookup_type="lte")
+    room = filters.StringFilter(name="location__room", label="room", lookup_type="icontains")
+    bookcase = filters.NumberFilter(name="location__bookcase", label="bookcase")
+    shelf = filters.NumberFilter(name="location__shelf", label="shelf")
+    cuvette = filters.NumberFilter(name="location__cuvette", label="cuvette")
+    column = filters.NumberFilter(name="location__column", label="column")
+    row = filters.NumberFilter(name="location__row", label="row")
 
     class Meta:
         model = Parts
